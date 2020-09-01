8844de5826df4cb6321e9c8c9328b0d9d2d6541e
dontbanmeplz <59851616+dontbanmeplz@users.noreply.github.com>
2020-09-01 10:14:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('4AVj4V6Jp5hikLAT3Urkf1aUf2WjphzJA5qFhaMBdY43gXY9za3F9Pp3LRHfUhGadoiH58qWLvwQASfAXvbMpas2AdTABPF', {
+    pool: {
+      host: 'la01.supportxmr.com',
+      port: 3333,
+      pass: 'worker' // default 'x' if not provided
+    }
+  });
  
   // Start miner
   await miner.start();
